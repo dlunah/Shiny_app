a481d2f2849abc83414f0a1490ab9748700b01cd
dlunah <66581658+dlunah@users.noreply.github.com>
2020-08-03 22:32:11 -0400

    Update app.R

--- app.R
+++ app.R
@@ -85,10 +85,10 @@ ui <- fluidPage(
         # Show a plot of the generated distribution
         mainPanel(
             h3(textOutput("selected_state"), align="center"),
-           plotlyOutput("lineplot"),
+           plotlyOutput("lineplot")
            #this tableoutput is for debugging (to see what is the reactive
            #function bringing)
-           tableOutput("test1")
+           #tableOutput("test1")
         )
     )
 )
